d39c3a0a1827dabeb798a8f3da1b9becc172c2ee
Jonathan <djgrovezone@gmail.com>
2022-07-22 19:40:38 +0200

    Agregamos contenido a header con material design

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,49 @@
+import { AuthService } from 'src/app/shared/services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'firebase/auth';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  providers: [AuthService]
 })
 export class HeaderComponent implements OnInit {
+//Administración -------------------
+  public debug: boolean = false;
+//----------------------------------
+  //user: any;
+  isLogged: boolean = false;
+  isLoading: boolean = false;
+  ruta!: string;
+  public user!: User | null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router, public auth: AuthService) {
+    //if(this.debug)console.log(auth.getCurrentUser());
   }
 
+  async ngOnInit() {}
+
+  goBack(){    
+    this.router.navigate(['home']);
+  }
+
+  goLogin(){    
+    this.router.navigate(['/']);
+  }
+
+  goRegister(){    
+    this.router.navigate(['/register']);
+  }
+
+  goToProfile(){    
+    this.router.navigate(['/profile']);
+  }
+
+  logout(){
+    this.auth.logout()
+    .then(() => this.router.navigate(['/']))
+    .catch((e) => console.error(e.message));
+  }
 }
--- src/app/shared/components/header/header.module.ts
+++ src/app/shared/components/header/header.module.ts
@@ -1,12 +1,24 @@
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatProgressBarModule
+  ],
+  exports: [HeaderComponent],
 })
-export class HeaderModule { }
+export class HeaderModule {}
